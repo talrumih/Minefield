d30be2c508e5d8b675f207ef56878083deca1fbf
talrumih <th999th@gmail.com>
2016-09-26 11:56:13 -0700

    added some tests

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,31 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+TEST(FieldTest, placeMineOutBounds)
+{
+	Field minefield;
+	minefield.placeMine(200, 111);
+        ASSERT_EQ( MINE_HIDDEN, minefield.get(200,111) );
+}
+
+TEST(FieldTest, Safe)
+{
+	Field minefield;
+	minefield.placeMine(1,1);
+	ASSERT_TRUE(minefield.isSafe(2,2));
+
+}
+
+TEST(FieldTest, NotSafe)
+{
+        Field minefield;
+        minefield.placeMine(1,1);
+        ASSERT_FALSE(minefield.isSafe(1,1));
+}
+
+TEST(FieldTest, getOutBounds)
+{
+	Field minefield;
+	ASSERT_ANY_THROW(minefield.get(111,122));
+} 
